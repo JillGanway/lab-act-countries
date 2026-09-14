57b7db7f701896833f373d268236ccf341f28eb8
JillGanway <jilliansgl17@gmail.com>
2026-09-14 18:39:29 +0800

    Task 6 and 7: add 404 page and fix JSX

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
         <Route path="countries" element={<CountriesPage />} />
         <Route path="bucket-list" element={<BucketListPage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
